cfd4dfc19bb3b97997787fd694a236af5956c123
Sergey Nekoval <s.nekoval@mangotele.com>
2015-09-28 20:06:57 +0300

    image load

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var apiKey = '[YOUR API KEY]';
 
+$.getJSON("http://api.flickr.com/services/feeds/groups_pool.gne?id=675729@N22&lang=en-us&format=json&jsoncallback=?", function(data){
+  $.each(data.items, function(i,item){
+    $("<img/>").attr("u", "image").attr("src", item.media.m).appendTo("#images")
+      .wrap("<div></div>");
+  });
+});
